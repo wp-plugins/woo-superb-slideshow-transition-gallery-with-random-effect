850122f5b750c7de2f61358bfb56bc669742cb55
gopiplus <gopiplus@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-05-10 13:36:27 +0000

    git-svn-id: https://plugins.svn.wordpress.org/woo-superb-slideshow-transition-gallery-with-random-effect/trunk@542395 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ help.php
@@ -0,0 +1,12 @@
+<strong>1. Drag and drop the widget!</strong><br />
+1.	Go to widget menu and drag and drop the "Woo superb slideshow transition gallery with random effect" widget to your sidebar location. or <br />
+<br /><strong>2.Paste the below code to your desired template location!</strong><br />
+2.	Copy and past the below mentioned code to your desired template location(ie in PHP file).
+<div style="padding-top:7px;padding-bottom:7px;"> <code style="padding:7px;"> &lt;?php if (function_exists (woo_show)) woo_show(); ?&gt; </code></div>
+<br /><strong>3.Use below code in post or page!</strong><br />
+<div style="padding-top:7px;padding-bottom:7px;">
+  <input name="" style="width:400px;height:25px;" value="[woo-superb-slideshow=widget=YES]" type="text" />
+</div>
+In above code "widget" is your gallery type, "YES" is random option.<br />
+<br />
+Check official website for live demo and more information <a target="_blank" href='http://www.gopiplus.com/work/2010/09/19/woo-superb-slideshow-transition-gallery-with-random-effect/'>click here</a>
---
+++ woo-superb-slideshow-transition-gallery-with-random-effect.js
@@ -0,0 +1,111 @@
+/**
+ *     Woo superb slideshow transition gallery with random effect
+ *     Copyright (C) 2012  www.gopiplus.com
+ * 
+ *     This program is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     This program is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
+var global_transitions=[ //array of IE transition strings
+	"progid:DXImageTransform.Microsoft.Barn()",
+	"progid:DXImageTransform.Microsoft.Blinds()",
+	"progid:DXImageTransform.Microsoft.CheckerBoard()",
+	"progid:DXImageTransform.Microsoft.Fade()",
+	"progid:DXImageTransform.Microsoft.GradientWipe()",
+	"progid:DXImageTransform.Microsoft.Inset()",
+	"progid:DXImageTransform.Microsoft.Iris()",
+	"progid:DXImageTransform.Microsoft.Pixelate(maxSquare=15)",
+	"progid:DXImageTransform.Microsoft.RadialWipe()",
+	"progid:DXImageTransform.Microsoft.RandomBars()",
+	"progid:DXImageTransform.Microsoft.RandomDissolve()",
+	"progid:DXImageTransform.Microsoft.Slide()",
+	"progid:DXImageTransform.Microsoft.Spiral()",
+	"progid:DXImageTransform.Microsoft.Stretch()",
+	"progid:DXImageTransform.Microsoft.Strips()",
+	"progid:DXImageTransform.Microsoft.Wheel()",
+	"progid:DXImageTransform.Microsoft.Zigzag()"
+]
+
+function woo_target(setting){
+	this.wrapperid=setting.wrapperid
+	this.imagearray=setting.imagearray
+	this.pause=setting.pause
+	this.transduration=setting.transduration/1000 //convert from miliseconds to seconds unit to pass into el.filters.play()
+	this.currentimg=0
+	var preloadimages=[] //temp array to preload images
+	for (var i=0; i<this.imagearray.length; i++){
+		preloadimages[i]=new Image()
+		preloadimages[i].src=this.imagearray[i][0]
+	}
+	document.write('<div id="'+this.wrapperid+'" class="'+setting.wrapperclass+'"><div id="'+this.wrapperid+'_inner" style="width:100%">'+this.getSlideHTML(this.currentimg)+'</div></div>')
+	var effectindex=Math.floor(Math.random()*global_transitions.length) //randomly pick a transition to utilize
+	var contentdiv=document.getElementById(this.wrapperid+"_inner")
+	if (contentdiv.filters){ //if the filters[] collection is defined on element (only in IE)
+		contentdiv.style.filter=global_transitions[effectindex] //define transition on element
+		this.pause+=setting.transduration //add transition time to pause
+	}
+	this.filtersupport=(contentdiv.filters && contentdiv.filters.length>0)? true : false //test if element supports transitions and has one defined
+	var slideshow=this
+	woo_target.addEvent(contentdiv, function(){slideshow.isMouseover=1}, "mouseover")
+	woo_target.addEvent(contentdiv, function(){slideshow.isMouseover=0}, "mouseout")
+	setInterval(function(){slideshow.rotate()}, this.pause)
+}
+
+woo_target.addEvent=function(target, functionref, tasktype){
+	if (target.addEventListener)
+		target.addEventListener(tasktype, functionref, false);
+	else if (target.attachEvent)
+		target.attachEvent('on'+tasktype, function(){return functionref.call(target, window.event)});
+},
+
+woo_target.setopacity=function(el, degree){ //sets opacity of an element (FF and non IE browsers only)
+	if (typeof el.style.opacity!="undefined")
+		el.style.opacity=degree
+	else
+		el.style.MozOpacity=degree
+	el.currentopacity=degree
+},
+
+woo_target.prototype.getSlideHTML=function(index){
+	var slideHTML=(this.imagearray[index][1])? '<a href="'+this.imagearray[index][1]+'" target="'+this.imagearray[index][2]+'">\n' : '' //hyperlink slide?
+	slideHTML+='<img src="'+this.imagearray[index][0]+'" />'
+	slideHTML+=(this.imagearray[index][1])? '</a><br />' : '<br />'
+	slideHTML+=(this.imagearray[index][3])? this.imagearray[index][3] : '' //text description?
+	return slideHTML //return HTML for the slide at the specified index
+}
+
+woo_target.prototype.rotate=function(){
+	var contentdiv=document.getElementById(this.wrapperid+"_inner")
+	if (this.isMouseover){ //if mouse is over slideshow
+		return
+	}
+	this.currentimg=(this.currentimg<this.imagearray.length-1)? this.currentimg+1 : 0
+	if (this.filtersupport){
+		contentdiv.filters[0].apply()
+	}
+	else{
+		woo_target.setopacity(contentdiv, 0)
+	}
+	contentdiv.innerHTML=this.getSlideHTML(this.currentimg)
+	if (this.filtersupport){
+		contentdiv.filters[0].play(this.transduration)
+	}
+	else{
+		contentdiv.fadetimer=setInterval(function(){
+			if (contentdiv.currentopacity<1)
+				woo_target.setopacity(contentdiv, contentdiv.currentopacity+0.1)
+			else
+				clearInterval(contentdiv.fadetimer)
+		}, 50) //end setInterval
+	}
+}
